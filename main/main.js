9cc741f8704759fb156388a51d5a237872524cea
Ivan Kay <strategetical@gmail.com>
2014-11-09 19:52:53 -0600

    0.1.0
    various garbage, still lots of improvements yet to be made

---
+++ main/main.js
@@ -0,0 +1,65 @@
+$(function(){
+    $("#main-audio").on("play", function(){
+        toggle_play_icon(true);
+    });
+    $("#main-audio").on("abort", function(){
+        toggle_play_icon(false);
+    });
+    $("#player .controls .play").on("click", function(){
+        toggle_play(true);
+    });
+    $("#player .controls .pause").on("click", function(){
+        toggle_play(false);
+    });
+    $("#volume-control").on("input", function(){
+        $("#main-audio")[0].volume = this.value/100;
+    });
+    $("#player .controls .mute").on("click", function(){
+        toggle_mute(true);
+    });
+    $("#player .controls .unmute").on("click", function(){
+        toggle_mute(false);
+    });
+    setInterval(updateMetadata, 2000);
+    updateMetadata();
+});
+
+function toggle_play_icon(playing){
+    if (playing){
+        $("#player .controls .pause").show();
+        $("#player .controls .play").hide();
+    }
+    else{
+        $("#player .controls .pause").hide();
+        $("#player .controls .play").show();
+    }
+}
+
+function toggle_play(play){
+    if (play){
+        $("#main-audio")[0].src = "http://radio.ivan.moe:420/vaporwave-main?"+Date.now();
+        $("#main-audio")[0].play();
+    }
+    else{
+        $("#main-audio")[0].pause();
+        $("#main-audio")[0].src = "";
+    }
+}
+
+function toggle_mute(mute){
+    if (mute){
+        $("#player .controls .mute").hide();
+        $("#player .controls .unmute").show();
+    }
+    else{
+        $("#player .controls .mute").show();
+        $("#player .controls .unmute").hide();
+    }
+    $("#main-audio")[0].muted = mute;
+}
+
+function updateMetadata(){
+    $.get("http://radio.ivan.moe:420/status-json.xsl", function(a){
+        console.log(a);
+    });
+}

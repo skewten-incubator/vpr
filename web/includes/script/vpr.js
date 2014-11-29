68ee51780b3cf06c68b15217b71ad61995c8597a
Ivan Kay <strategetical@gmail.com>
2014-11-29 17:09:17 -0600

    tiny fixes
    finished the mobile player view

--- web/includes/script/vpr.js
+++ web/includes/script/vpr.js
@@ -1,18 +1,44 @@
+var player,
+    isPlaying = false;
+
 $(function(){
-    $("#main-audio").on("play", function(){
+    attach_handlers();
+    
+    load_player();
+
+    setInterval(update_metadata, 5000);
+    update_metadata();
+});
+
+function attach_handlers(){
+    var player = $("#main-audio");
+
+    player.on("play", function(){
         toggle_play_icon(true);
     });
-    $("#main-audio").on("abort", function(){
+    player.on("pause", function(){
         toggle_play_icon(false);
     });
+    player.on("error", function(){
+        isPlaying = false;
+        toggle_play_icon(false);
+        handle_player_error();
+    });
+    player.on("playing", function(){
+        isPlaying = true;
+        $(".vpr-player .artist").text("Loading info...");
+        update_metadata();
+    });
+
+    $("#volume-control").on("input", function(){
+        player[0].volume = this.value/100;
+    });
+
     $(".vpr-player .play").on("click", function(){
         toggle_play(true);
     });
     $(".vpr-player .pause").on("click", function(){
         toggle_play(false);
-    });
-    $("#volume-control").on("input", function(){
-        $("#main-audio")[0].volume = this.value/100;
     });
     $(".vpr-player .mute").on("click", function(){
         toggle_mute(true);
@@ -20,14 +46,12 @@ $(function(){
     $(".vpr-player .unmute").on("click", function(){
         toggle_mute(false);
     });
-    setInterval(updateMetadata, 5000);
-    updateMetadata();
-    loadPlayer();
-});
+}
 
-function loadPlayer(){
-    $("#main-audio")[0].volume = $("#volume-control").val()/100;
-    $("#main-audio")[0].play();
+function load_player(){
+    player = $("#main-audio")[0];
+    player.volume = $("#volume-control").val()/100;
+    toggle_play(true);
 }
 
 function toggle_play_icon(playing){
@@ -43,12 +67,14 @@ function toggle_play_icon(playing){
 
 function toggle_play(play){
     if (play){
-        $("#main-audio")[0].src = "/icecast/vaporwave-main?"+Date.now();
-        $("#main-audio")[0].play();
+        $(".vpr-player .error").removeClass("error");
+        $(".vpr-player .title").text("");
+        $(".vpr-player .artist").text("Loading stream...");
+        player.src = "http://radio.virtualplaza.tk/icecast/vpr-playlist?"+Date.now();
+        player.play();
     }
     else{
-        $("#main-audio")[0].pause();
-        $("#main-audio")[0].src = "";
+        player.pause();
     }
 }
 
@@ -61,13 +87,45 @@ function toggle_mute(mute){
         $(".vpr-player .mute").show();
         $(".vpr-player .unmute").hide();
     }
-    $("#main-audio")[0].muted = mute;
+    player.muted = mute;
 }
 
-function updateMetadata(){
-    $.get("/icecast/status-json.xsl", function(data){
+function update_metadata(){
+    if (!isPlaying){
+        return;
+    }
+    $(".vpr-player .artist.error").removeClass("error").text("Loading info...");
+    $.ajax({
+        type: "GET",
+        url: "/_/info"
+    }).done(function(data){
+        return;
+        //TODO
         var song = data.icestats.source.title.split(" - ");
-        $("#player .artist").text(song.shift());
-        $("#player .title").text(song.join(" - "));
+        $(".vpr-player .artist").text(song.shift());
+        $(".vpr-player .title").text(song.join(" - "));
+    }).fail(function(a, b, error){
+        $(
+            ".vpr-status .listeners,"+
+            ".vpr-status .kbps,"+
+            ".vpr-status .dj"
+        ).text("???");
+        $(".vpr-player .artist").addClass("error").text("Could not fetch info!");
+        console.error(error);
     });
+}
+
+function handle_player_error(){
+    if (player.src == window.location.href){
+        return;
+    }
+    var errors = {
+        1: "User aborted playback.",
+        2: "A network error occured.",
+        3: "Could not decode stream.",
+        4: "Could not connect to stream.",
+        5: "Unknown error."
+    };
+    $(".vpr-player .artist").addClass("error").text("An error occured!");
+    $(".vpr-player .title").addClass("error").text(errors[player.error.code]);
 }
